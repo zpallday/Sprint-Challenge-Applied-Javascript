77ad544cfe4cc4435765a3fa72622cdab02af0aa
zachary-peasley <zpman95@hotmail.com>
2019-08-09 11:04:21 -0500

    second  commit

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,30 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(res => {
+      cardS(res.data);
+  })
+  .catch(err => {
+      console.log("Error")
+  })
+
+  function cardS(data){
+      console.log(data);
+      
+      console.log(data['articles']['bootstrap']);
+      for(topic in data.articles){
+
+        data['articles']['topic'].forEach( e=> {
+            const card = document.createElement('div');
+            const headline = document.createElement('div');
+            const author = document.createElement('div');
+            const imgContainer = document.createElement('div');
+            const img = document.createElement('img');
+            const name = document.createElement('span');
+
+            
+        })
+      }
+  }
