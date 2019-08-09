66ffd91cc5e247c5248392610d8e40082d1b4a91
zachary-peasley <zpman95@hotmail.com>
2019-08-09 12:00:29 -0500

    second  commit

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,41 +6,90 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
-//
+{/* <div class="card">
+  <div class="headline">{Headline of article}</div>
+  <div class="author">
+    <div class="img-container">
+      <img src={url of authors image} />
+    </div>
+    <span>By {authors name}</span>
+  </div>
+</div> */}
+
 // Create a card for each of the articles and add the card to the DOM.
 
+
+
+
+const cardContainer = document.querySelector('.cards-container');
+
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
-  .then(res => {
-      cardS(res.data);
-  })
-  .catch(err => {
-      console.log("Error")
-  })
+.then(object => {
+  const items = object.data.articles.bootstrap
 
-  function cardS(data){
-      console.log(data);
-      
-      console.log(data['articles']['bootstrap']);
-      for(topic in data.articles){
+  items.forEach(article => {
+  cardContainer.append(createCard(article))
+ })
 
-        data['articles']['topic'].forEach( e=> {
-            const card = document.createElement('div');
-            const headline = document.createElement('div');
-            const author = document.createElement('div');
-            const imgContainer = document.createElement('div');
-            const img = document.createElement('img');
-            const name = document.createElement('span');
+   const js = object.data.articles.javascript
 
-            
-        })
-      }
-  }
+   js.forEach(article => {
+   cardContainer.append(createCard(article))
+
+    })
+
+    const jq = object.data.articles.jquery
+    jq.forEach(article => {
+    cardContainer.append(createCard(article))
+
+    })
+
+    const tech = object.data.articles.technology
+     tech.forEach(article => {
+     cardContainer.append(createCard(article))
+
+    })
+
+    const node = object.data.articles.node
+    node.forEach(article => {
+    cardContainer.append(createCard(article))
+
+    })
+
+})
+
+.catch(error => {
+  console.log('error', error)
+})
+
+function createCard(object){
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const photo = document.createElement('div');
+  const photo = document.createElement('img');
+  const credit = document.createElement('span');
+
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    photo.classList.add('img-container');
+
+
+
+    headline.textContent = object.headline;
+    photo.src = object.authorPhoto;
+    photo.alt = 'Photo of author';
+    credit.textContent = `By ${object.authorName}`;
+
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(photo);
+    photo.appendChild(photo);
+    author.appendChild(credit);
+
+return card;
+
+}
